402a82a3d2b3f149722fd082997bef280821887c
Diogenes Franca <diogenes15@msn.com>
2017-01-05 20:51:09 -0200

    Initial commit

---
+++ app/src/main/java/layout/MoviesFragment.java
@@ -0,0 +1,42 @@
+package layout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import app.com.example.android.popularmovies.R;
+
+public class MoviesFragment extends Fragment {
+
+    ArrayAdapter<ImageView> moviesAdapter;
+    public MoviesFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        moviesAdapter = new ArrayAdapter<ImageView>(
+                getActivity(),
+                R.layout.grid_item_movies,
+                R.id.grid_item_movies_imageview,
+                new ArrayList<ImageView>());
+
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+        final GridView moviesGridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        moviesGridView.setAdapter(moviesAdapter);
+        return rootView;
+    }
+}
