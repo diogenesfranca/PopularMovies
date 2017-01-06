835cb0aa65ae2352edcdcd9274e4f4d20905a4be
Diogenes Franca <diogenes15@msn.com>
2017-01-06 13:07:46 -0200

    Update MoviesFragment to use MovieAdapter

--- app/src/main/java/app/com/example/android/popularmovies/MoviesFragment.java
+++ app/src/main/java/app/com/example/android/popularmovies/MoviesFragment.java
@@ -5,15 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 public class MoviesFragment extends Fragment {
 
-    ArrayAdapter<ImageView> moviesAdapter;
+    MovieAdapter movieAdapter;
     public MoviesFragment() {
     }
 
@@ -25,16 +23,14 @@ public class MoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        moviesAdapter = new ArrayAdapter<ImageView>(
-                getActivity(),
-                R.layout.grid_item_movies,
-                R.id.grid_item_movies_imageview,
-                new ArrayList<ImageView>());
-
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
-        final GridView moviesGridView = (GridView) rootView.findViewById(R.id.gridview_movies);
-        moviesGridView.setAdapter(moviesAdapter);
+
+        movieAdapter = new MovieAdapter(getActivity(), new ArrayList<Movie>());
+
+        GridView moviesGridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        moviesGridView.setAdapter(movieAdapter);
+        
         return rootView;
     }
 }
