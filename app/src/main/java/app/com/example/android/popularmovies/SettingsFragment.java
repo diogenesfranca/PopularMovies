73af5aa6f38cf334b2b8dbbe7d21d7add36b066a
Diogenes Franca <diogenes15@msn.com>
2017-01-08 10:35:35 -0200

    Change SettingsFragment to display the chosen setting

--- app/src/main/java/app/com/example/android/popularmovies/SettingsFragment.java
+++ app/src/main/java/app/com/example/android/popularmovies/SettingsFragment.java
@@ -2,9 +2,12 @@ package app.com.example.android.popularmovies;
 
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
 
     public SettingsFragment() {
@@ -17,5 +20,38 @@ public class SettingsFragment extends PreferenceFragment {
 
         //Load the preferences from a XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.preferences_search_type_key)));
+    }
+
+    /**
+     *Attaches a listener so the summary is always updated with the preference value.
+     *Also fires the listener once, to initialize the summary (so it shows up before the value
+     *is changed.)
+     */
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if(preference instanceof ListPreference){
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(stringValue);
+            if(index >= 0){
+                preference.setSummary(listPreference.getEntries()[index]);
+            }
+        }
+        return true;
     }
 }
